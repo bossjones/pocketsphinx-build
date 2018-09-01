37d1d9c548dc0bea0db5fdd81ab7f526653b0955
Malcolm Jones <bossjones@theblacktonystark.com>
2018-09-01 13:49:12 -0400

    WIP: livedemo

--- init_pyenv.sh
+++ init_pyenv.sh
@@ -4,11 +4,11 @@ env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.7.0
 
 # pyenv virtualenv 3.7.0 pocketsphinx-build37
 
-PYENV_DEBUG=1 pyenv virtualenv --system-site-packages 3.7.0 pocketsphinx-build37
+pyenv virtualenv --system-site-packages 3.7.0 pocketsphinx-build37
 
 pyenv activate pocketsphinx-build37
 
-env PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig" pip install pygobject==3.28.3 ptpython black isort ipython pdbpp
+env PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig" pip install pygobject==3.28.3 ptpython black isort ipython pdbpp Pillow matplotlib numpy_ringbuffer MonkeyType
 
 export LD_LIBRARY_PATH=/usr/local/lib
 # export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
