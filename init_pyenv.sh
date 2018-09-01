527203399e6de7e20d61ad60fac3c593ccdabcdc
Malcolm Jones <malcolm@adobe.com>
2018-09-01 10:47:14 -0400

    wip: manifest

--- init_pyenv.sh
+++ init_pyenv.sh
@@ -5,3 +5,7 @@ env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.7.0
 # pyenv virtualenv 3.7.0 pocketsphinx-build37
 
 PYENV_DEBUG=1 pyenv virtualenv --system-site-packages 3.7.0 pocketsphinx-build37
+
+pyenv activate pocketsphinx-build37
+
+env PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig" pip install pygobject==3.28.3 ptpython black isort
